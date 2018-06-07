146ef55b2b2ccfe5b9e215f9594d54ce7b226c00
Santosh Ugale <santosh_ugale@nuance.com>
2018-06-07 15:35:41 +0530

    Added message service

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -2,15 +2,17 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { MessagesService } from './messages.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  public constructor() { }
+  public constructor(private messageService: MessagesService) { }
 
   public getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
 }
